88c73757586cbb6529bce54764017be40b68c776
Fue Idriss Shulu <61651516+Jitomorin@users.noreply.github.com>
2024-10-12 17:19:04 +0300

    More changes

--- components/BlogContainer.tsx
+++ components/BlogContainer.tsx
@@ -1,4 +1,4 @@
-export function BlogContainer({ children }: any) {
+export default function BlogContainer({ children }: any) {
   return <div className="container mx-auto px-5 md:px-0">{children}</div>;
 }
 
